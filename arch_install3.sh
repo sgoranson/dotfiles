e22aaf86c41a2a781c57d361bdcde752c84cf5b9
Steve Goranson <steve@stevegoranson.com>
2019-02-28 21:39:53 -0500

    dopeassshit

--- arch_install3.sh
+++ arch_install3.sh
@@ -35,15 +35,15 @@ parted ${strInstallDrive} rm 5 --script || true
 parted ${strInstallDrive} rm 6 --script || true
 dd if=/dev/zero of=${strInstallDrive} bs=512 count=10
 
-# parted ${strInstallDrive} mklabel gpt
-# parted ${strInstallDrive} mkpart primary fat32 1MiB ${strPartitionSizeBoot}
-parted ${strInstallDrive} mklabel msdos --script
+parted ${strInstallDrive} mklabel gpt --script
+parted ${strInstallDrive} mkpart primary fat32 1MiB 100MiB --script
+# parted ${strInstallDrive} mklabel msdos --script
 #parted ${strInstallDrive} mkpart primary ext4 1MiB ${strPartitionSizeBoot}
 
-parted ${strInstallDrive}  mkpart primary ext4 1MiB 100MiB --script
-parted ${strInstallDrive}  set 1 boot on --script
-parted ${strInstallDrive}  mkpart primary ext4 100MiB 90% --script
-parted ${strInstallDrive}  mkpart primary linux-swap 90% 100% --script
+# parted ${strInstallDrive}  mkpart primary ext4 1MiB 100MiB --script
+# parted ${strInstallDrive}  set 1 boot on --script
+parted ${strInstallDrive}  mkpart primary ext4 100MiB 100% --script
+# parted ${strInstallDrive}  mkpart primary linux-swap 90% 100% --script
 
 
 echo -e ${infoColor}"END OF PARTITIONING"
@@ -51,26 +51,18 @@ echo -e ${outputColor}
 sleep 3
 
 #Format the partitions
-# mkfs.vfat -F32 ${strInstallDrive}1
-mkfs.ext4 ${strInstallDrive}1
+mkfs.vfat -F32 ${strInstallDrive}1
+# mkfs.ext4 ${strInstallDrive}1
 mkfs.ext4 ${strInstallDrive}2
-mkswap ${strInstallDrive}3
-swapon ${strInstallDrive}3
+# mkswap ${strInstallDrive}3
+# swapon ${strInstallDrive}3
 sync
 
 #Mount the partitions
 mount ${strInstallDrive}2 /mnt
-cd /mnt
-rm -rf *
-mkdir -p /mnt/boot
-mkdir -p /mnt/home
-mount ${strInstallDrive}1 /mnt/boot
-cd /mnt/boot
-rm -rf *
-# mount ${strInstallDrive}4 /mnt/home
-# cd /mnt/home
-# rm -rf *
-# cd ~
+mkdir -p /mnt/efi
+mount ${strInstallDrive}1 /mnt/efi
+
 echo -e ${infoColor}"END OF FORMATTING"
 sleep 3
 
@@ -91,7 +83,16 @@ sleep 3
 
 #Generate an fstab
 echo -e ${infoColor}"GENERATING FSTAB"
-genfstab -U -p /mnt >> /mnt/etc/fstab
+genfstab -U  /mnt >> /mnt/etc/fstab
+
+#Set timezone
+echo -e ${infoColor}"START OF SETTING TIMEZONE"
+echo -e ${outputColor}
+arch-chroot /mnt /bin/bash -c "ln -s /usr/share/zoneinfo/${strTimezone} /etc/localtime"
+arch-chroot /mnt hwclock --systohc 
+echo -e ${infoColor}"END OF SETTING TIMEZONE"
+sleep 3
+
 
 #Set locale
 echo -e ${infoColor}"START OF SETTING LOCALE"
@@ -102,13 +103,6 @@ arch-chroot /mnt /bin/bash -c "echo LANG=${strLanguage} > /etc/locale.conf"
 echo -e ${infoColor}"END OF SETTING LOCALE"
 sleep 3
 
-#Set timezone
-echo -e ${infoColor}"START OF SETTING TIMEZONE"
-echo -e ${outputColor}
-arch-chroot /mnt /bin/bash -c "ln -s /usr/share/zoneinfo/${strTimezone} /etc/localtime"
-arch-chroot /mnt hwclock --systohc --utc
-echo -e ${infoColor}"END OF SETTING TIMEZONE"
-sleep 3
 
 #Set hostname
 echo -e ${infoColor}"START OF SETTING HOSTNAME"
@@ -120,16 +114,16 @@ sleep 3
 #Install the bootloader
 echo -e ${infoColor}"START OF BOOTLOADER INSTALLATION"
 echo -e ${outputColor}
-# arch-chroot /mnt pacman -S dosfstools
-# arch-chroot /mnt bootctl --path=/boot install
-# arch-chroot /mnt /bin/bash -c 'echo "title Arch Linux" >> /boot/loader/entries/arch.conf\'
-# arch-chroot /mnt /bin/bash -c 'echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf\'
-# arch-chroot /mnt /bin/bash -c 'echo "initrc /initramfs-linux.img" >> /boot/loader/entries/arch.conf\'
-# arch-chroot /mnt /bin/bash -c $'echo "options root=${strInstallDrive}1 rw resume=${strInstallDrive}3" >> /boot/loader/entries/arch.conf\'
-# arch-chroot /mnt /bin/bash -c 'echo "timeout 0" > /boot/loader/loader.conf\' #There is only 1 > because the file is created on install, and were overwriting it
-# arch-chroot /mnt /bin/bash -c 'echo "default arch" >> /boot/loader/loader.conf\'
-arch-chroot /mnt grub-install --target=i386-pc --recheck ${strInstallDrive}
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt pacman -S dosfstools
+arch-chroot /mnt bootctl --path=/boot install
+arch-chroot /mnt /bin/bash -c 'echo "title Arch Linux" >> /boot/loader/entries/arch.conf\'
+arch-chroot /mnt /bin/bash -c 'echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf\'
+arch-chroot /mnt /bin/bash -c 'echo "initrc /initramfs-linux.img" >> /boot/loader/entries/arch.conf\'
+arch-chroot /mnt /bin/bash -c $'echo "options root=${strInstallDrive}1 rw resume=${strInstallDrive}3" >> /boot/loader/entries/arch.conf\'
+arch-chroot /mnt /bin/bash -c 'echo "timeout 0" > /boot/loader/loader.conf\' #There is only 1 > because the file is created on install, and were overwriting it
+arch-chroot /mnt /bin/bash -c 'echo "default arch" >> /boot/loader/loader.conf\'
+# arch-chroot /mnt grub-install --target=i386-pc --recheck ${strInstallDrive}
+# arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e ${infoColor}"END OF BOOTLOADER INSTALLATION"
 sleep 3
